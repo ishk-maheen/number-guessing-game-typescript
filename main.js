689378ad345b4767c846706a355e09b015593640
MAHEEN SHEIKH <160344462+ishk-maheen@users.noreply.github.com>
2024-05-31 02:10:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Guess a number between 1-10"
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congrats, you guessed right number!");
+}
+else {
+    console.log("Sorry! you guessed wrong number!");
+}
+;
